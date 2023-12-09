2b9e49bcfdf07129e228daf373f9813d2b344a08
Edielson Assis <edielsonnice@hotmail.com>
2023-12-09 09:25:12 -0300

    Projeto criado

---
+++ src/main/java/com/proz/projetointegrador/ProjetointegradorApplication.java
@@ -0,0 +1,13 @@
+package com.proz.projetointegrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetointegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetointegradorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proz/projetointegrador/ProjetointegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proz.projetointegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetointegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
