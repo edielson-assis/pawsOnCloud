6dcf95b876591e2190991cdf93033784f3bead99
Edielson Assis <edielsonnice@hotmail.com>
2024-01-06 13:57:35 -0300

    Criado os DTOs para entidade Doacao

---
+++ src/main/java/br/com/pawsoncloud/dto/DoacaoDto.java
@@ -0,0 +1,5 @@
+package br.com.pawsoncloud.dto;
+
+import jakarta.validation.Valid;
+
+public record DoacaoDto(@Valid AnimaisDto pet) {}
---
+++ src/main/java/br/com/pawsoncloud/dto/DoacaoUpdateDto.java
@@ -0,0 +1,14 @@
+package br.com.pawsoncloud.dto;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+
+public record DoacaoUpdateDto(
+
+    @NotNull
+    Long id,
+
+    @Valid
+    AnimaisUpdateDto pet) {
+    
+}
